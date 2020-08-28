7c16f0fa05526aa9ffc66e6b271e520e1d64226b
Aldahir <aldahir5010@gmail.com>
2020-08-28 11:30:17 -0500

    registro de clientes, citas, productos

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -3,14 +3,20 @@ from django.db import models
 # Create your models here.
 class Clientes(models.Model):
     nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
-    email=models.EmailField()
-    tfno=models.CharField(max_length=10)
+    direccion=models.CharField(max_length=50, verbose_name="La direcciòn")
+    email=models.EmailField(blank=True, null=True)
+    tfno=models.CharField(max_length=10, verbose_name="Telefono")
+
+    def __str__(self):
+        return self.nombre
 
 class Articulos(models.Model):
     nombre=models.CharField(max_length=30)
     seccion=models.CharField(max_length=20)
     precio=models.IntegerField()
+
+    def __str__(self):
+        return 'El nomre es %s la secciòn es %s y el precio es %s' % (self.nombre, self.seccion, self.precio)
 
 class Pedidos(models.Model):
     numero=models.IntegerField()
